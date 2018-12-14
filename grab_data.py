2a9e8acdff69309ee5e9d8d37948d1df9f4653bf
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2018-12-14 11:25:29 +0100

    - move to more pythonic way of writing things - introduce helper 'debug_few_elements' that allow preview flow - replace imperative with declarative style of modification - optimize memory consumption by replacing lists with generators

--- grab_data.py
+++ grab_data.py
@@ -3,9 +3,9 @@ scrapper danych z głównej strony Wykop.pl
 '''
 
 import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-
+from bs4 import BeautifulSoup as bs
+from itertools import chain
+import csv
 
 '''
 funkcja z bloku dla jednego znaleziska wydziela gromadzone informacje:
@@ -17,75 +17,62 @@ funkcja z bloku dla jednego znaleziska wydziela gromadzone informacje:
 - link = dokąd prowadzi znalezisko?
 - opis = opis znaleziska
 '''
+
+
 def grab_wykop_item_data(fitem):
-	wykopy = fitem.select("a.ajax span")
-	if(len(wykopy) != 0):
-		wykopy = int(wykopy[0].get_text())
-		autor = fitem.select("div.fix-tagline a.affect")[0].get_text()
-		data = fitem.select("div.row.elements span.affect time")[0]["datetime"]
-		tytul =  fitem.select("h2 a")[0].get_text().strip()
-		link = fitem.select("div.fix-tagline span.tag.create a.affect")[0]["href"]
-		opis = fitem.select("div.description p.text a")[0].get_text().strip()
+    wykopy = fitem.select("a.ajax span")
+    if len(wykopy) != 0:
+        wykopy = int(wykopy[0].get_text())
+        autor = fitem.select("div.fix-tagline a.affect")[0].get_text()
+        data = fitem.select("div.row.elements span.affect time")[0]["datetime"]
+        tytul = fitem.select("h2 a")[0].get_text().strip()
+        link = fitem.select("div.fix-tagline span.tag.create a.affect")[0]["href"]
+        opis = fitem.select("div.description p.text a")[0].get_text().strip()
 
-		tagi = fitem.select("div.fix-tagline a.tag.affect.create")
-		tags = []
-		for tag in tagi:
-			temp_tag = tag.get_text()
-			if temp_tag[0] == "#":
-				tags.append(temp_tag)
+        tagi = fitem.select("div.fix-tagline a.tag.affect.create")
+        tags = []
+        for tag in tagi:
+            temp_tag = tag.get_text()
+            if temp_tag[0] == "#":
+                tags.append(temp_tag)
 
-		return True, wykopy, autor, data, tags, tytul, link, opis
+        return True, wykopy, autor, data, tags, tytul, link, opis
 
-	return False,
-
-# pusta ramka danych na zebrane dane
-# dane = pd.DataFrame()
+    return False,
 
 
-# weź pierwsze 3000 stron z Wykop.pl
-for i in range(1, 3000, 1):
-	# budujemy urla
-	page_url = f"https://www.wykop.pl/strona/{i}/"
-	print(f"{page_url}")
+urls = [
+    "https://www.wykop.pl/strona/{}".format(page)
+    for page in range(1, 3000, 1)
+]
 
-	# wczytujemy kolejną stronę
-	page = requests.get(page_url)
-
-	# parser strony
-	soup = BeautifulSoup(page.content, 'html.parser')
-
-	# znajdujemy wszystkie elemeny "li"
-	items = soup.find_all(("li", ["link", "iC"]))
-
-	# dla każdego z nich
-	for item in items:
-		# jaką ma klasę?
-		try:
-			clazz = item["class"]
-		except KeyError:
-			clazz = ""
-
-		# jeśli jest iC wśród klas to zbieramy to co w środku
-		if "iC" in clazz:
-			dane_temp = grab_wykop_item_data(item)
-
-			if dane_temp[0]:
-				dane_temp = {'wykopy': dane_temp[1],
-							'autor': dane_temp[2],
-							'data': dane_temp[3],
-							'tags': dane_temp[4],
-							'tytul': dane_temp[5],
-							'link': dane_temp[6],
-							'opis': dane_temp[7]
-							}
-				dane_temp = pd.DataFrame(dane_temp)
-
-				# dopisz do pliku z danymi
-				dane_temp.to_csv("plik.csv", mode='a', header = False)
-
-				# dodaj do pełnej ramki danych
-#				dane = dane.append(dane_temp, ignore_index = True)
+pages = (requests.get(url) for url in urls)
+contents = (page.content for page in pages)
+soups = (bs(content, 'html.parser') for content in contents)
+lists_of_items = (soup.select('li.iC') for soup in soups)  # just li with class iC
+all_items_together = chain.from_iterable(lists_of_items)
+extracted = (grab_wykop_item_data(item) for item in all_items_together)
+filter_out_nones = (item for item in extracted if item is not None)  # Skip Nones
 
 
-# dane.to_csv("plik.csv")
-# dane.to_json("plik.json")
+def write_to_csv():
+    with open('wykop.csv', 'w+') as destination_file:
+        writer = csv.writer(destination_file, delimiter=';', quotechar='"')
+
+        for row in filter_out_nones:
+            writer.writerow(row)
+
+
+def debug_few_elements():
+    print(next(filter_out_nones))
+    print(next(filter_out_nones))
+    print(next(filter_out_nones))
+    print(next(filter_out_nones))
+
+
+def transform_to_list():
+    return list(filter_out_nones)  # danger, load whole content to memory, fine for few items
+
+
+if __name__ == '__main__':
+    debug_few_elements()
